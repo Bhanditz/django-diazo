6bfe77528a8d630f18cf74a05ea46c5565ebcf53
Bryan Wilson <bryanlandia@gmail.com>
2018-01-05 19:50:55 -0800

    Bugfix:apps.py handle import of AppConfig subclasses in INSTALLED_APPS

--- django_diazo/apps.py
+++ django_diazo/apps.py
@@ -17,12 +17,23 @@ class DjangoDiazoConfig(AppConfig):
         """
         for app in settings.INSTALLED_APPS:
             try:
-                app_path = __import__(app, {}, {}, [app.split('.')[-1]]).__path__
-            except AttributeError:
-                continue
+                app_path = __import__(app, {}, {}, [str(app.split('.')[-1])]).__path__
+            except ImportError:
+                # might be an AppConfig subclass, walk up dotted path to find a directory-level module
+                # that might contain a `MODULE_NAME` module
+                app = app.split('.')
+                while app:               
+                    try:
+                        del app[-1]
+                        app_path = __import__('.'.join(app), {}, {}, [app[-1]]).__path__
+                        break
+                    except (AttributeError, ImportError):
+                        continue
+                    except IndexError:
+                        break
 
             try:
                 imp.find_module(MODULE_NAME, app_path)
-            except ImportError:
+            except (ImportError, NameError):
                 continue
             __import__('%s.%s' % (app, MODULE_NAME))
